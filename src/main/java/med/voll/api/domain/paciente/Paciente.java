08b77a6e7569d5cb5502d78053f513453549b3d1
Paulo Roberto Antunes De Carvalho <paulorantunes13@hotmail.com>
2024-06-18 21:06:57 -0300

    Feat: Tratando as Respostas de error da Requisição

--- src/main/java/med/voll/api/domain/paciente/Paciente.java
+++ src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -1,16 +1,11 @@
-package med.voll.api.paciente;
+package med.voll.api.domain.paciente;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import med.voll.api.endereco.DadosEndereco;
-import med.voll.api.endereco.Endereco;
-import org.springframework.format.annotation.NumberFormat;
+import med.voll.api.domain.endereco.Endereco;
 
 @Entity(name="Paciente")
 @Table(name = "pacientes")
