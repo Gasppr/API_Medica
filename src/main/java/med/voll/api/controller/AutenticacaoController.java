3f484e0d5048f6cf000468464737e12f8b8cdab9
Paulo Roberto Antunes De Carvalho <paulorantunes13@hotmail.com>
2024-06-20 11:24:10 -0300

    Feat: Adicionando a camada Filter de segurança da API.
    Description: Adicionando a camada Filter na API para a autenticação do usuário via login(email e senha) , para a geração token e verificação no banco de dados.

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -3,6 +3,9 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.DadosAutenticacao;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.DadosTokenJWT;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
@@ -24,13 +27,18 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+@Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dadosAutenticacao){
 
-            var token = new UsernamePasswordAuthenticationToken(dadosAutenticacao.login(), dadosAutenticacao.senha());
-            var authentication = manager.authenticate(token);
+            var authenticationToken = new UsernamePasswordAuthenticationToken(dadosAutenticacao.login(), dadosAutenticacao.senha());
+            var authentication = manager.authenticate(authenticationToken);
 
-            return ResponseEntity.ok().build();
+            var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+            return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
 
     }
 
